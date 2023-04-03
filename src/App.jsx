173529466d7b657d41381554ad6810fa442a3d95
Hemel Bourhan <hemelbourhan@gmailgit config --global user.email Bourhan>
2023-04-04 00:27:04 +0600

    create navBar component and set an array in this conponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,9 +8,7 @@ function App() {
   return (
     <div className="App">
       <h1 className=" text-4xl bg-purple-600">Responsive-nav-with-react-and-tailwind</h1>
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
+      <Navbar></Navbar>
     </div>
   );
 }
